14f5097707cc8017cb1369d9548c188c1ef9170c
Noriaki Sakai <noriaki.sakai.develop@gmail.com>
2015-12-03 17:48:17 +0900

    Initial Commit

---
+++ QuizApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizApp
+//
+//  Created by 酒井紀明 on 2015/12/03.
+//  Copyright © 2015年 noriaki.sakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuizApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuizApp
+//
+//  Created by 酒井紀明 on 2015/12/03.
+//  Copyright © 2015年 noriaki.sakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
